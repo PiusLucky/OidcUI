99cbffff42fbc9f010306f830f58aefcb440ddee
luckypius50 <luckypius50@gmail.com>
2022-01-31 11:34:40 +0100

    Commit modifications

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
+// In a normal production setting, this details will be saved in .env (or environment variable)
 const oidcConfig = {
   onSignIn: async (user) => {
     window.location = '/home';
@@ -6,10 +7,16 @@ const oidcConfig = {
     window.location = '/';
   },
   autoSignIn: false,
-  authority: process.env.REACT_APP_AUTHORITY,
-  clientId: process.env.REACDT_APP_CLIENT_ID,
-  redirectUri: `${process.env.REACT_APP_DOMAIN}/${process.env.REACT_APP_REDIRECT_URI}`,
-  scope: `${process.env.REACT_APP_SCOPE_1} ${process.env.REACT_APP_SCOPE_2} ${process.env.REACT_APP_SCOPE_3}`
+  authority: 'https://id-sandbox.cashtoken.africa/.well-known/openid-configuration',
+  clientId: 'wprQYMZBqqx-dgszFUfQG',
+  redirectUri: 'http://localhost:3000/oauth-callback',
+  scope: 'openid email profile'
+
+  //sewcure version
+  // authority: process.env.REACT_APP_AUTHORITY,
+  // clientId: process.env.REACDT_APP_CLIENT_ID,
+  // redirectUri: `${process.env.REACT_APP_DOMAIN}/${process.env.REACT_APP_REDIRECT_URI}`,
+  // scope: `${process.env.REACT_APP_SCOPE_1} ${process.env.REACT_APP_SCOPE_2} ${process.env.REACT_APP_SCOPE_3}`
 };
 
 export default oidcConfig;
